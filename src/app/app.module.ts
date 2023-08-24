31165be8209ec873f613dbc4be6c81bc5b763a89
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2023-08-23 19:47:47 -0600

    se agregan los recuadros paraa descargar los documentos, falta la funcionalidad y darle un mayor diseño a la pagina, el boton le falta diseño recuerda cambiarlo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SliderComponent } from './slider/slider.component';
+import { RoundedImagesComponent } from './rounded-images/rounded-images.component';
+import { DocumentosComponent } from './documentos/documentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SliderComponent,
+    RoundedImagesComponent,
+    DocumentosComponent
   ],
   imports: [
     BrowserModule
